f015d3ed6b6d858550f7884d93ae70d9317d65bc
Lukas Jonca <lukas.jonca.lj@gmail.com>
2024-05-13 15:01:28 +0300

    Added tutorial + styles

--- packages/nextjs/components/HowToPlay.tsx
+++ packages/nextjs/components/HowToPlay.tsx
@@ -1,3 +1,5 @@
+import "~~/styles/custom.css";
+
 export const HowToPlayComponent: React.FC = ({}) => {
   return (
     <div className="flex-1 flex flex-col h-full p-mono">
@@ -17,15 +19,25 @@ export const HowToPlayComponent: React.FC = ({}) => {
         <h2 className="text-xl pb-2" style={{ borderBottom: "0.5px solid white", textAlign: "center" }}>
           How to get Biocash
         </h2>
-        <h3>Find a chest</h3>
 
-        <img src="/FindAChest.png"></img>
-        <h3>Move your character next to the chest</h3>
-        <img src="/NextToTheChest.png"></img>
-        <h3>Press F on the chest to open it</h3>
+        <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "flex-start", alignItems: "row" }}>
+          <div className="img-container">
+            <img src="/FindAChest.png" className="tutorial-img"></img>
+            <h3>1. Find a chest</h3>
+          </div>
 
-        <img src="/Inventory.png"></img>
-        <h3>Drag Dirt into it</h3>
+          <div className="img-container">
+            <img src="/NextToTheChest.png" className="tutorial-img"></img>
+            <h3>2. Move your character next to the chest</h3>
+          </div>
+
+          <div className="img-container">
+            <img src="/Inventory.png" className="tutorial-img"></img>
+            <h3>3. Press F on the chest to open it</h3>
+            <h3>4. Drag Dirt into it</h3>
+          </div>
+        </div>
+
         <h3>Now you have Biocash!</h3>
       </div>
     </div>
