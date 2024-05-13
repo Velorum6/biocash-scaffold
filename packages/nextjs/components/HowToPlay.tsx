8af338c0809464fe4fbe1f3f6fcbc086dc504a13
Velorum <88809256+Velorum6@users.noreply.github.com>
2024-05-13 13:05:37 +0200

    Added Tutorial
    HowToPlay.tsx

--- packages/nextjs/components/HowToPlay.tsx
+++ packages/nextjs/components/HowToPlay.tsx
@@ -15,8 +15,18 @@ export const HowToPlayComponent: React.FC = ({}) => {
       </div>
       <div className="p-12 flex flex-col justify-between gap-4">
         <h2 className="text-xl pb-2" style={{ borderBottom: "0.5px solid white", textAlign: "center" }}>
-          Your Instructions Here
+          How to get Biocash
         </h2>
+        <h3>Find a chest</h3>
+
+        <img src="/FindAChest.png"></img>
+        <h3>Move your character next to the chest</h3>
+        <img src="/NextToTheChest.png"></img>
+        <h3>Press F on the chest to open it</h3>
+
+        <img src="/Inventory.png"></img>
+        <h3>Drag Dirt into it</h3>
+        <h3>Now you have Biocash!</h3>
       </div>
     </div>
   );
